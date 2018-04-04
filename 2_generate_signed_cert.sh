6601b82cb27df1b48d02fabeefedf20d3f6ae09f
Jason Vanderhoof <jvanderhoof@gmail.com>
2018-04-04 06:19:31 -0600

    Added counter for issuing certs

--- 2_generate_signed_cert.sh
+++ 2_generate_signed_cert.sh
@@ -5,7 +5,10 @@ echo ' -- Sign client certificate (valid for 10 seconds) --'
 docker cp ssh-client:/root/.ssh/id_rsa.pub .
 
 # Generate a cert that's valid for 10 seconds
-ssh-keygen -s ca-certs/ca -I user_root -n root -V +10s id_rsa.pub
+ssh-keygen -s ca-certs/ca -I user_root -n root -V +10s -z $(cat counter) id_rsa.pub
+
+# Increment the cert counter
+echo "$(($(cat counter) + 1))" > counter
 
 docker cp id_rsa-cert.pub ssh-client:/root/.ssh/
 docker-compose exec ssh-client chown root:root /root/.ssh/id_rsa-cert.pub
--- 4_generate_zoned_signed_cert.sh
+++ 4_generate_zoned_signed_cert.sh
@@ -4,7 +4,10 @@ echo 'Generate a signed public key for the `zone-webservers` Zone'
 # Sign client public key
 docker cp ssh-client:/root/.ssh/id_rsa.pub .
 
-ssh-keygen -s ca-certs/ca -I user_root -n zone-webservers -V +60m -z 1 id_rsa.pub
+ssh-keygen -s ca-certs/ca -I user_root -n zone-webservers -V +60m -z $(cat counter) id_rsa.pub
+
+# Increment the cert counter
+echo "$(($(cat counter) + 1))" > counter
 
 docker cp id_rsa-cert.pub ssh-client:/root/.ssh/
 rm -f id_rsa-cert.pub id_rsa.pub
